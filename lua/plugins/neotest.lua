11c5f022ac29f74fafbb79e311b043e3ed2b23c7
Jonatan Witoszek <jonatanwitoszek@gmail.com>
2024-11-19 17:08:51 +0100

    Fix neotest, fix unimpaired keymaps

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -14,5 +14,39 @@ return {
         "neotest-python",
       },
     },
+    config = function(_, opts)
+      if opts.adapters then
+        local adapters = {}
+        for name, config in pairs(opts.adapters or {}) do
+          if type(name) == "number" then
+            if type(config) == "string" then config = require(config) end
+            adapters[#adapters + 1] = config
+          elseif config ~= false then
+            local adapter = require(name)
+            if type(config) == "table" and not vim.tbl_isempty(config) then
+              local meta = getmetatable(adapter)
+              if adapter.setup then
+                adapter.setup(config)
+              elseif meta and meta.__call then
+                adapter(config)
+              else
+                error("Adapter " .. name .. " does not support setup")
+              end
+            end
+            adapters[#adapters + 1] = adapter
+          end
+        end
+        opts.adapters = adapters
+      end
+      vim.diagnostic.config({
+        virtual_text = {
+          format = function(diagnostic)
+            local message = diagnostic.message:gsub("\n", " "):gsub("\t", " "):gsub("%s+", " "):gsub("^%s+", "")
+            return message
+          end,
+        },
+      }, vim.api.nvim_create_namespace "neotest")
+      require("neotest").setup(opts)
+    end,
   },
 }
--- lua/plugins/unimpaired-nvim.lua
+++ lua/plugins/unimpaired-nvim.lua
@@ -15,26 +15,26 @@ return {
           description = "Add [count] blank lines below",
           dot_repeat = true,
         },
-        exchange_above = {
-          mapping = "[e",
-          description = "Exchange line with [count] lines above",
-          dot_repeat = true,
-        },
-        exchange_below = {
-          mapping = "]e",
-          description = "Exchange line with [count] lines below",
-          dot_repeat = true,
-        },
-        exchange_section_above = {
-          mapping = "[e",
-          description = "Move section [count] lines up",
-          dot_repeat = true,
-        },
-        exchange_section_below = {
-          mapping = "]e",
-          description = "Move section [count] lines down",
-          dot_repeat = true,
-        },
+        -- exchange_above = {
+        --   mapping = "[e",
+        --   description = "Exchange line with [count] lines above",
+        --   dot_repeat = true,
+        -- },
+        -- exchange_below = {
+        --   mapping = "]e",
+        --   description = "Exchange line with [count] lines below",
+        --   dot_repeat = true,
+        -- },
+        -- exchange_section_above = {
+        --   mapping = "[e",
+        --   description = "Move section [count] lines up",
+        --   dot_repeat = true,
+        -- },
+        -- exchange_section_below = {
+        --   mapping = "]e",
+        --   description = "Move section [count] lines down",
+        --   dot_repeat = true,
+        -- },
         previous_file = {
           mapping = "[f",
           description = "Previous file in directory. :colder in qflist",
